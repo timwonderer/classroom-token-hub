61d4874b7d2db36dfdf178772744d5a41fd8af8c
Timothy Chang <76604824+timwonderer@users.noreply.github.com>
2026-03-08 17:30:18 -0700

    Merge economy policy and membership migration heads

---
+++ migrations/versions/b9c0d1e2f3a4_merge_economy_policy_and_membership_heads.py
@@ -0,0 +1,22 @@
+"""merge economy policy and membership heads
+
+Revision ID: b9c0d1e2f3a4
+Revises: a11213ca4afb, f8a9b0c1d2e3
+Create Date: 2026-03-09 09:25:00.000000
+
+"""
+
+
+# revision identifiers, used by Alembic.
+revision = "b9c0d1e2f3a4"
+down_revision = ("a11213ca4afb", "f8a9b0c1d2e3")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
